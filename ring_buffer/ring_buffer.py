c1c5c9f6e0deb80e6e8ce8b67609ded8b0140c2f
olympia <owojcik7@gmail.com>
2020-02-28 13:27:38 -0500

    Complete get method for ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,21 +1,69 @@
 from doubly_linked_list import DoublyLinkedList
 
+"""
+- Non-growable, fixed size
+
+- append --> #Adds elements to the buffer
+- When full, the OLDEST el is overwritten by a new el
+Check for length of storage vs capacity - 
+
+- get --> #returns all elements in the buffer in order, do not return any NONE values in list even if present in buffer
+
+We are using a DLL for storage
+
+In this example, head is the last added item
+Tail is the most recently added item 
+
+"""
 
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.current = None #where head of ring is currently located
         self.storage = DoublyLinkedList()
-
+    
     def append(self, item):
-        pass
+        # print('current', self.current)
+        # print('capacity', self.capacity)
+        # print('storage', self.storage)
 
+        #if storage is 0, initiate ring
+        if self.storage.length == 0:
+            self.storage.add_to_head(item)
+            #set current to that item
+            self.current = self.storage.head
+
+        #if at capacity and current is NOT at head. remove from head & add to tail
+        elif self.storage.length == self.capacity and not self.current is self.storage.head:
+            #remove the head and add to tail item
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+        #if at capacity and current is at head (end of list)
+        elif self.storage.length == self.capacity:
+            #remove item the head and add to tail item
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            #set current to tail
+            self.current = self.storage.tail
+        #if not at capacity, add to tail
+        else:
+            self.storage.add_to_tail(item)
+           
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        # if self.current is None:
+        #     return list_buffer_contents
+        while len(list_buffer_contents) < self.storage.length:
+            list_buffer_contents.append(self.current.value)
+            #if there is next, set current to next
+            if self.current.next:
+                self.current = self.current.next
+            else:
+                #set current to head
+                self.current = self.storage.head
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
@@ -30,3 +78,22 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+buffer = RingBuffer(3)
+# print("Print Buffer get() ---> ", buffer.get())        # []
+buffer.append('a')
+# # print("Print Buffer get() ---> ", buffer.get())        # 1 ['a']
+buffer.append('b')
+# # print("Print Buffer get() ---> ", buffer.get())        # 2 ['a', 'b']
+# buffer.append('c')
+# # print("Print Buffer get() ---> ", buffer.get())        # should return ['a', 'b', 'c']
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+# # print("Print Buffer get() ---> ", buffer.get())        # should return ['d', 'b', 'c']
+# buffer.append('e')
+# # print("Print Buffer get() ---> ", buffer.get())        # should return ['d', 'e', 'c']
+# buffer.append('f')
+# buffer.append('g')
+# buffer.append('h')
+# buffer.append('i')
+# print("Print Buffer get() ---> ", buffer.get())        # should return ['d', 'e', 'f']
